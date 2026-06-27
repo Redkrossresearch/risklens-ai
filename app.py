b311c8951d6a154b29aa23075b433c4aa0779b03
Abiha <sayyedabiha85@gmail.com>
2026-06-27 17:25:54 +0530

    Updated AI Risk Analysis app

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 from langchain_community.llms import Ollama
-
 llm = Ollama(model="qwen3:1.7b")
 
-prompt = """
-You are a Cyber Security Risk Analyst.
+prompt = input("Enter vulnerability details: ")
 
-Analyze the following vulnerability.
-
-Title: SMB Signing Disabled
-Severity: Critical
-Description:
-SMB signing is disabled on the server.
-
-Provide:
-1. Risk Title
-2. Business Impact
-3. Likelihood
-4. Risk Rating
-5. Remediation
-"""
-
-response = llm.invoke(prompt)
+response = llm.invoke(
+    f"""
+    Analyze this security vulnerability:
+    
+    {prompt}
+    
+    Give:
+    1. Risk Title
+    2. Business Impact
+    3. Likelihood
+    4. Risk Rating
+    5. Remediation
+    """
+)
 
 print(response)
