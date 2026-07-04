943258ef33aaabd979be88b8c4af81ddcd60e087
Tanishka Shirke <tanishkashirke17@gmail.com>
2026-07-04 20:52:39 +0530

    Added Risk Heatmap dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
- import { BrowserRouter, Routes, Route } from "react-router-dom";
- import Sidebar from "./components/Sidebar";
- import UploadPage from "./pages/UploadPage";
- import DashboardPage from "./pages/DashboardPage";
+ import { Button } from "@/components/ui/button";
 
- export default function App() {
- return (
- <BrowserRouter>
- <div className="flex h-screen bg-gray-50">
- <Sidebar />
- <main className="flex-1 overflow-auto p-6">
- <Routes>
- <Route path="/" element={<UploadPage />} />
- <Route path="/dashboard" element={<DashboardPage />} />
- </Routes>
- </main>
- </div>
- </BrowserRouter>
-);
+function App() {
+  return (
+    <Button>Click Me</Button>
+  );
 }
+
+export default App;
+
