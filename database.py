45972b7156e76bd1ae66fcfb970e60dbbd02e609
prasadsunilhalankar-sys <prasadsunilhalankar@gmail.com>
2026-07-11 10:41:35 +0530

    Update database URL to Supabase

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:risklens@localhost:5432/risklens_db"
+DATABASE_URL = "postgresql://postgres:prasad@180526@db.ozugufxusjdysqmgdjwt.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
