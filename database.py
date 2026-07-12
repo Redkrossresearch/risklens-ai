58518eeb1f4e660719477a95bc17165b96f81592
prasadsunilhalankar-sys <prasadsunilhalankar@gmail.com>
2026-07-12 10:40:54 +0530

    Fix Supabase connection - use transaction pooler

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:prasad%40180526@db.ozugufxusjdysqmgdjwt.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres.ozugufxusjdysqmgdjwt:prasad%40180526@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
